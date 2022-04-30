33140279d06dbabd8e0262213a62469c8a3d1ddc
nahomdebretsion <nahomdebretsion@gmail.com>
2022-04-30 11:45:46 -0500

    ssss

--- tips.js
+++ tips.js
@@ -3,5 +3,15 @@ function calcTip() {
     let tip = document.getElementById('tip').value;
     let total = (parseFloat(subtotal) * parseFloat(tip) / 100) + parseFloat(subtotal);
     document.getElementById('total').innerText = '$' + total;
+    // <!-- <script>
+    //     function calcTip() {
+    //         let subtotal = document.getElementById('subtotal').value;
+    //         let tip = document.getElementById('tip').value;
+    //         let total = (parseFloat(subtotal) * parseFloat(tip) / 100) + parseFloat(subtotal);
+    //         document.getElementById('total').innerText = '$' + total;
+    //     }
+
+    // </script> -->
+
 }
 
